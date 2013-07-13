c2d7b42fb78242eb8bff4563f3922f96e4f3ec66
toshi <me@toshimaru.net>
2013-07-13 00:19:47 -0700

    add explanation

--- demo/van3927130767/js/main.js
+++ demo/van3927130767/js/main.js
@@ -3,8 +3,8 @@
   "use strict";
   $(function(){
     $('.info').on('click', function(){
-      $(this).find('table').toggle('slow');
-      $(this).find('p').toggle('slow');
+      $(this).find('table').slideToggle('slow');
+      $(this).find('p').slideToggle('slow');
       $('html, body').animate({
        scrollTop: $(this).offset().top
       }, 500);
