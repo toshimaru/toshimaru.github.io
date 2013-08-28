65854caf562365dc4ef7dbd35b4e13d5522265ac
toshi <me@toshimaru.net>
2013-08-28 01:45:03 -0700

    add files for grunt

---
+++ about/Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      css: {
+        files: 'css/*.css',
+        options: {
+          livereload: true,
+        },
+      },
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+};
