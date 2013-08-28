e54c387cc666b2939bc0b5f77867c13c9dc5a515
toshi <me@toshimaru.net>
2013-08-27 22:42:23 -0700

    Add images and diet HTML file

--- about/js/main.js
+++ about/js/main.js
@@ -1,13 +1,31 @@
-$(function(){
+(function(){
+  var userAgent = window.navigator.userAgent.toLowerCase();
 
-  $('section[data-type="background"]').each(function(){
-    var $bgobj = $(this);
+  var device_type = 'desktop';
+  if(userAgent.indexOf('ipad') > 0 ||
+     userAgent.indexOf('iphone') > 0 ||
+     userAgent.indexOf('ipod') > 0 ||
+     userAgent.indexOf('android') > 0) {
+     var win = $(window);
+     if (win.width() > win.height()) {
+         device_type = 'tablet';
+     } else {
+         device_type = 'phone';
+     }
+  }
 
-    $(window).scroll(function() {
-      var yPos = -($(window).scrollTop() / $bgobj.data('speed'));
-      var coords = '50% '+ yPos + 'px';
-      $bgobj.css({ backgroundPosition: coords });
+  $(function(){
+    $('section[data-type="background"]').each(function(){
+      var $bgobj = $(this);
+
+      $(window).scroll(function() {
+        var yPos = -($(window).scrollTop() / $bgobj.data('speed'));
+        var coords = '50% '+ yPos + 'px';
+        $bgobj.css({ backgroundPosition: coords });
+      });
     });
   });
+  
+})();
 
-});
+
