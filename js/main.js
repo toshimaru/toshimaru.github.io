7d5fa0db70c95502fd9cbd82788d9244afdf7f9b
toshi <me@toshimaru.net>
2013-10-06 02:15:58 -0700

    Add random colored background feature

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-
 $(function(){
-  $('#top').fadeIn(1500);
+  "use strict";
+
+  var colors = ['lightblue', 'lightpink', 'lightgreen', 'lightsalmon'];
+
+  $('body').css('background-color', sample(colors));
+  $('#top').fadeIn(1200);
+
+  function sample(ary) {
+    return ary[Math.floor(Math.random() * ary.length)];
+  }
 });
