65cc55def51ec536a708cf3ae87f373f549f217f
toshi <me@toshimaru.net>
2013-12-06 23:00:26 -0800

    Use CSS animation instead of jQuery animation

--- js/main.js
+++ js/main.js
@@ -3,10 +3,9 @@ $(function(){
 
   var colors = ['lightblue', 'lightpink', 'lightgreen', 'lightsalmon'];
 
-  $('body').css('background-color', sample(colors));
-  $('#top').fadeIn(1200);
-
   function sample(ary) {
     return ary[Math.floor(Math.random() * ary.length)];
   }
+
+  $('body').css('background-color', sample(colors));
 });
